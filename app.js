d09cd22307810a4097b86f7a138075d84212865c
Logan Martins <logan.iphone97@gmail.com>
2024-06-04 15:17:59 -0300

    de 10 pra 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];  
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto =gerarNumeroAleatorio();
 let tentativas = 1 
 
@@ -10,7 +10,7 @@ function exibirTextoNaTela (tag, texto) {
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do numero secreto');
-    exibirTextoNaTela('p', 'escolha um numero entre 1 e 10');
+    exibirTextoNaTela('p', 'escolha um numero entre 1 e 100');
  }
 
 exibirMensagemInicial();
